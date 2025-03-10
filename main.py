782b48382e71ffb9aa185f2702dd11feeb264c4c
MariaMsschs <maria.msschs@gmail.com>
2025-03-10 17:51:26 -0300

    merge com a main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from cnn import CnnReader
+from sources.cnn import CnnReader
 from sources.g1 import G1Reader
 
 report = {
@@ -14,12 +14,10 @@ report = {
 def main():
     driver = webdriver.Chrome()
     
-    cnnReader = CnnReader(driver)
-    
-    cnnReader.noticias_cnn
+    # cnnReader = CnnReader(driver)
+    # cnnReader.noticias_cnn
 
     g1_reader = G1Reader(driver)
-    
     g1_reader.attatch_news(report)
 
 print(report)
