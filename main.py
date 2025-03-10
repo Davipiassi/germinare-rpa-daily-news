3f66820a537258066f64baf84c552271305a360d
MariaMsschs <maria.msschs@gmail.com>
2025-03-10 18:09:13 -0300

    g1 e cnn ok

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ report = {
 def main():
     driver = webdriver.Chrome()
     
-    # cnnReader = CnnReader(driver)
-    # cnnReader.noticias_cnn
+    cnnReader = CnnReader(driver)
+    cnnReader.noticias_cnn(report)
 
     g1_reader = G1Reader(driver)
     g1_reader.attatch_news(report)
+
+main()
 
 print(report)
